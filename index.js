55dd274b8678ef8b148f6abc25991a270b4a0545
occua <josuahmmanurng170807@gmail.com>
2026-07-08 08:04:44 +0700

    testing edit code 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Halo! Ini deployment via Jenkins CI/CD - Versi 1');
+  res.send('Halo! Ini deployment via Jenkins CI/CD - Versi 2 (auto-deploy berhasil!)');
 });
 
 app.listen(PORT, () => {
